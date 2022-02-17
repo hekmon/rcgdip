193d39c4e9f43a4a9461314b7c4c81d8e9c4cfc0
Hekmon <edouardhur@gmail.com>
2022-02-17 21:33:39 +0100

    move signals related fx to their own file

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/hekmon/rcgdip/drivechange"
 	"github.com/hekmon/rcgdip/gdrive"
@@ -137,44 +135,4 @@ func main() {
 	<-mainStop
 	logger.Debugf("[Main] clean stop ok, exiting")
 	os.Exit(exitCode)
-}
-
-func handleSignals() {
-	// Register signals
-	var sig os.Signal
-	signalChannel := make(chan os.Signal, 3)
-	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGINT, syscall.SIGUSR1)
-	// Waiting for signals to catch
-	for sig = range signalChannel {
-		switch sig {
-		case syscall.SIGUSR1:
-			backupDB()
-		case syscall.SIGTERM:
-			fallthrough
-		case syscall.SIGINT:
-			logger.Output("\n")
-			logger.Infof("[Main] signal '%v' caught: initiating clean stop", sig)
-			mainCtxCancel()
-			return
-		default:
-			logger.Warningf("[Main] Signal '%v' caught but no process set to handle it: skipping", sig)
-		}
-	}
-}
-
-func backupDB() {
-	// Systemd notify
-	if err := sysdnotify.Reloading(); err != nil {
-		logger.Errorf("[Main] can't send systemd reloading notification: %v", err)
-	} else if systemdLaunched {
-		logger.Debug("[Main] systemd reloading notification sent")
-	}
-	// Backup db
-	db.Backup()
-	// Systemd notify
-	if err := sysdnotify.Ready(); err != nil {
-		logger.Errorf("[Main] can't send systemd ready notification: %v", err)
-	} else if systemdLaunched {
-		logger.Debug("[Main] systemd ready notification sent")
-	}
 }
